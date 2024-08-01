b8721b9da3a5e2f9f8464630922881227c3e5906
Muhamad Mupid Ahmadiawan <133787157+Yuefii@users.noreply.github.com>
2024-08-01 21:35:53 +0700

    reusable components activity

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,5 +1,4 @@
 import { FaInstagram } from "react-icons/fa"
-import { MdOutlineMapsHomeWork } from "react-icons/md"
 import { TfiEmail } from "react-icons/tfi"
 
 export const navitems = [
@@ -32,5 +31,23 @@ export const contactitems = [
         title: "Pos Bitung, Jl. Raya Serang No.Km. 10, Kadu Jaya, Kec. Curug, Kabupaten Tangerang, Banten 15810",
         href: "https://www.google.com/maps/place/Universitas+Insan+Pembangunan+Indonesia/@-6.2224685,106.5578429,17z/data=!3m1!4b1!4m6!3m5!1s0x2e69fe74eed2a5d1:0x10c2f57db3bc5d23!8m2!3d-6.2224685!4d106.5604232!16s%2Fg%2F1pzv6krgv?entry=ttu",
         icon: ""
+    },
+]
+
+export const activity = [
+    {
+        img: "/1.jpeg",
+        title: "Pelatihan Rutin",
+        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque voluptatibus magnam culpa dignissimos! Voluptatem dignissimos tenetur corporis ullam nostrum repellat est. Nam atque ab minus minima ad, voluptas qui rem!"
+    },
+    {
+        img: "/2.jpeg",
+        title: "Workshop Arduino",
+        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque voluptatibus magnam culpa dignissimos! Voluptatem dignissimos tenetur corporis ullam nostrum repellat est. Nam atque ab minus minima ad, voluptas qui rem!"
+    },
+    {
+        img: "/3.jpeg",
+        title: "Penerimaan Anggota Baru LDKO",
+        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque voluptatibus magnam culpa dignissimos! Voluptatem dignissimos tenetur corporis ullam nostrum repellat est. Nam atque ab minus minima ad, voluptas qui rem!"
     },
 ]
--- src/pages/kegiatan.tsx
+++ src/pages/kegiatan.tsx
@@ -1,11 +1,45 @@
+import { activity } from '@/constants';
+import Image from 'next/image';
 import React from 'react'
 
 export default function Kegiatan() {
     return (
         <main>
-            <div className='h-screen flex items-center justify-center'>
-                <h1 className='-mt-56'>COMMING SOON</h1>
+            <div className="mx-auto max-w-lg text-center py-24">
+                <h2 className="text-4xl mb-6 uppercase">Kegiatan Acara Kami</h2>
+                <p className="text-md">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
             </div>
+            {activity.map((item, index) => {
+                const isEvenIndex = (index + 1) % 2 === 0;
+                return (
+                    <div
+                        key={index}
+                        className="container mx-auto px-6 grid grid-cols-1 lg:grid-cols-2 lg:mb-24">
+                        <div className="relative h-52 md:h-72 lg:h-auto">
+                            <div>
+                                <Image
+                                    className="absolute rounded-md inset-0 w-full h-full object-cover"
+                                    src={item.img}
+                                    alt={item.title}
+                                    fill />
+                            </div>
+                        </div>
+                        <div className={
+                            `px-6 py-12 md:p-16 lg:p-32 flex items-center justify-center 
+                            ${isEvenIndex ?
+                                'order-last md:order-first' : 'order-last'}`}>
+                            <div>
+                                <h3
+                                    className="text-4xl mb-6 font-display text-black leading-tight">
+                                    {item.title}
+                                </h3>
+                                <p className='text-muted-foreground'>{item.desc}</p>
+                            </div>
+                        </div>
+                    </div>
+                )
+            }
+            )}
         </main>
     )
 }
