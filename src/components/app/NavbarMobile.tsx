11b22ec720508f7199800f09e42012ef9c16b1d2
Yuefii <muffidahmadiawan31@gmail.com>
2024-09-07 09:35:58 +0700

    chore: adding animation on homepages

--- src/components/app/NavbarMobile.tsx
+++ src/components/app/NavbarMobile.tsx
@@ -10,9 +10,9 @@ interface ToggleNavbarProps {
 const NavbarMobile: React.FC<ToggleNavbarProps> = ({ toggleContactInfo, toggleMobileMenu }) => {
     return (
         <div className="md:hidden fixed inset-0 w-full h-full flex flex-col items-center justify-center bg-primary z-40 uppercase space-y-4 text-xl tracking-widest transition-all text-center text-white">
-            <Link href="/tentang-kami">Tentang Kami</Link>
-            <Link href="/kegiatan">Kegiatan</Link>
-            <Link href="/struktur">Struktur</Link>
+            <Link onClick={toggleMobileMenu} href="/tentang-kami">Tentang Kami</Link>
+            <Link onClick={toggleMobileMenu} href="/kegiatan">Kegiatan</Link>
+            <Link onClick={toggleMobileMenu} href="/struktur">Struktur</Link>
             <button
                 onClick={toggleContactInfo}
                 type="button"
